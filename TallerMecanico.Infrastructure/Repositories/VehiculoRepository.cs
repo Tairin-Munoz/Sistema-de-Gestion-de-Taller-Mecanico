c40773439be5b8a9ec4b05f5b80c6a7582add901
Tairin-Munoz <tairinmunoz14@gmail.com>
2026-03-25 12:21:28 -0400

    feat: implementación del repositorio de vehiculos

---
+++ TallerMecanico.Infrastructure/Repositories/VehiculoRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TallerMecanico.Core.Entities;
+using TallerMecanico.Core.Interfaces;
+
+namespace TallerMecanico.Infrastructure.Repositories;
+
+public class VehiculoRepository : IVehiculoRepository
+{
+    private static List<Vehiculo> vehiculos = new();
+
+    public List<Vehiculo> GetAll() => vehiculos;
+
+    public Vehiculo GetById(int id) =>
+        vehiculos.FirstOrDefault(v => v.Id == id);
+
+    public void Add(Vehiculo vehiculo)
+    {
+        vehiculos.Add(vehiculo);
+    }
+}
