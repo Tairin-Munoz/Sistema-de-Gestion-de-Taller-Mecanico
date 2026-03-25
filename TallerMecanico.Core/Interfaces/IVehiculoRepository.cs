10d669b6b59db9f5968e86eaa22bdd17b2478d35
Tairin-Munoz <tairinmunoz14@gmail.com>
2026-03-25 12:17:17 -0400

    feat: creación de interfaz IVehiculoRepository

---
+++ TallerMecanico.Core/Interfaces/IVehiculoRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TallerMecanico.Core.Entities;
+
+namespace TallerMecanico.Core.Interfaces;
+
+public interface IVehiculoRepository
+{
+    List<Vehiculo> GetAll();
+    Vehiculo GetById(int id);
+    void Add(Vehiculo vehiculo);
+}
