2cd46bdfb002f068cbdc5de083fb1a592005694a
Tairin-Munoz <tairinmunoz14@gmail.com>
2026-03-25 14:41:40 -0400

    feat: configuración de entidad Vehiculo

--- TallerMecanico.Infrastructure/Repositories/VehiculoRepository.cs
+++ TallerMecanico.Infrastructure/Repositories/VehiculoRepository.cs
@@ -5,20 +5,32 @@ using System.Text;
 using System.Threading.Tasks;
 using TallerMecanico.Core.Entities;
 using TallerMecanico.Core.Interfaces;
+using TallerMecanico.Infrastructure.Data;
 
 namespace TallerMecanico.Infrastructure.Repositories;
 
 public class VehiculoRepository : IVehiculoRepository
 {
-    private static List<Vehiculo> vehiculos = new();
+    private readonly AppDbContext _context;
 
-    public List<Vehiculo> GetAll() => vehiculos;
+    public VehiculoRepository(AppDbContext context)
+    {
+        _context = context;
+    }
 
-    public Vehiculo GetById(int id) =>
-        vehiculos.FirstOrDefault(v => v.Id == id);
+    public List<Vehiculo> GetAll()
+    {
+        return _context.Vehiculos.ToList();
+    }
+
+    public Vehiculo GetById(int id)
+    {
+        return _context.Vehiculos.Find(id);
+    }
 
     public void Add(Vehiculo vehiculo)
     {
-        vehiculos.Add(vehiculo);
+        _context.Vehiculos.Add(vehiculo);
+        _context.SaveChanges();
     }
 }
