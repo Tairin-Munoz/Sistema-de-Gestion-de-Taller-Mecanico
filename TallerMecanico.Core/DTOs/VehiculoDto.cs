708583de74c2ade4dc73e03b2abfb7e05fea2a4b
Tairin-Munoz <tairinmunoz14@gmail.com>
2026-03-25 12:19:44 -0400

    feat: creación de DTO VehiculoDto

---
+++ TallerMecanico.Core/DTOs/VehiculoDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TallerMecanico.Core.DTOs;
+public class VehiculoDto
+{
+    public string Placa { get; set; }
+    public string Marca { get; set; }
+    public string Modelo { get; set; }
+    public int Anio { get; set; }
+}
