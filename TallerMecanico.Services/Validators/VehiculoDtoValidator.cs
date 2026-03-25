f4519bfd1ac280154db77b70bc2bd45746ca58ab
Tairin-Munoz <tairinmunoz14@gmail.com>
2026-03-25 14:42:59 -0400

    feat: implementación de validaciones

---
+++ TallerMecanico.Services/Validators/VehiculoDtoValidator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using TallerMecanico.Core.DTOs;
+
+namespace TallerMecanico.Services.Validators;
+
+public class VehiculoDtoValidator : AbstractValidator<VehiculoDto>
+{
+    public VehiculoDtoValidator()
+    {
+        RuleFor(x => x.Placa).NotEmpty();
+        RuleFor(x => x.Marca).NotEmpty();
+        RuleFor(x => x.Modelo).NotEmpty();
+        RuleFor(x => x.Anio).GreaterThan(1900);
+    }
+}
