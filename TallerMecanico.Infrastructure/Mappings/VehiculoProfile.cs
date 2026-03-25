0e42f08095b814d77db03e878af6dc5051c2eaf6
Tairin-Munoz <tairinmunoz14@gmail.com>
2026-03-25 12:22:41 -0400

    feat: configuración de AutoMapper para vehiculos

---
+++ TallerMecanico.Infrastructure/Mappings/VehiculoProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TallerMecanico.Core.DTOs;
+using TallerMecanico.Core.Entities;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace TallerMecanico.Infrastructure.Mappings;
+
+public class VehiculoProfile : Profile
+{
+    public VehiculoProfile()
+    {
+        CreateMap<Vehiculo, VehiculoDto>().ReverseMap();
+    }
+}
