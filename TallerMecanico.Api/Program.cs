ef92f3dc488fd9cc969d28c17d6fe839dca42757
Tairin-Munoz <tairinmunoz14@gmail.com>
2026-03-30 22:41:23 -0400

    feat: configuración final de Program.cs con DI, EF Core, AutoMapper y FluentValidation

--- TallerMecanico.Api/Program.cs
+++ TallerMecanico.Api/Program.cs
@@ -1,38 +1,40 @@
 using Microsoft.EntityFrameworkCore;
-using TallerMecanico.Core.Interfaces;
-using TallerMecanico.Infrastructure.Repositories;
 using TallerMecanico.Infrastructure.Data;
+using TallerMecanico.Infrastructure.Repositories;
 using TallerMecanico.Infrastructure.Mappings;
+using TallerMecanico.Core.Interfaces;
+using TallerMecanico.Services.Interfaces;
+using TallerMecanico.Services.Services;
 using TallerMecanico.Services.Validators;
 using FluentValidation;
 using FluentValidation.AspNetCore;
-using TallerMecanico.Services.Interfaces;
-using TallerMecanico.Services.Services;
-
-
-builder.Services.AddTransient<IVehiculoService, VehiculoService>();
-builder.Services.AddTransient<IVehiculoService, VehiculoService>();
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer("Server=.;Database=TallerMecanicoDb;Trusted_Connection=True;"));
+builder.Services.AddDbContext<TallerMecanicoContext>(options =>
+    options.UseSqlServer("Server=.;Database=TallerMecanicoDb;Trusted_Connection=True;TrustServerCertificate=True;")
+);
 
-builder.Services.AddScoped<IVehiculoRepository, VehiculoRepository>();
+builder.Services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+
+builder.Services.AddScoped<IVehiculoService, VehiculoService>();
 
 builder.Services.AddAutoMapper(typeof(VehiculoProfile));
 
 builder.Services.AddFluentValidationAutoValidation();
-builder.Services.AddValidatorsFromAssemblyContaining<VehiculoDtoValidator>();
+builder.Services.AddValidatorsFromAssemblyContaining<CrearVehiculoDtoValidator>();
 
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
+
+app.UseAuthorization();
 
 app.MapControllers();
 
