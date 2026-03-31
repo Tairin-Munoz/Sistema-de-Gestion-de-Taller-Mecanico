25831ae411091e3e986f82aaa5131e38484c1905
Tairin-Munoz <tairinmunoz14@gmail.com>
2026-03-30 21:52:50 -0400

    feat: creación de interfaz IVehiculoService

---
+++ TallerMecanico.Services/Interfaces/IVehiculoService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TallerMecanico.Core.Entities;
+
+namespace TallerMecanico.Services.Interfaces;
+
+public interface IVehiculoService
+{
+    Task<IEnumerable<Vehiculo>> GetAll();
+    Task<Vehiculo> GetById(int id);
+    Task Create(Vehiculo vehiculo);
+    Task Update(Vehiculo vehiculo);
+    Task Delete(int id);
+}
