3d52eff1d8a91e015a1a6aa91887639575140945
Tairin-Munoz <tairinmunoz14@gmail.com>
2026-03-30 21:53:59 -0400

    feat: implementación de VehiculoService con lógica de negocio

---
+++ TallerMecanico.Services/Services/VehiculoService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TallerMecanico.Core.Entities;
+using TallerMecanico.Core.Interfaces;
+using TallerMecanico.Services.Interfaces;
+
+namespace TallerMecanico.Services.Services;
+
+public class VehiculoService : IVehiculoService
+{
+    private readonly IVehiculoRepository _repository;
+
+    public VehiculoService(IVehiculoRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<IEnumerable<Vehiculo>> GetAll()
+    {
+        return await _repository.GetAll();
+    }
+
+    public async Task<Vehiculo> GetById(int id)
+    {
+        return await _repository.GetById(id);
+    }
+
+    public async Task Create(Vehiculo vehiculo)
+    {
+        // 🔥 REGLA DE NEGOCIO
+        if (string.IsNullOrEmpty(vehiculo.Placa))
+            throw new Exception("La placa es obligatoria");
+
+        await _repository.Add(vehiculo);
+    }
+
+    public async Task Update(Vehiculo vehiculo)
+    {
+        await _repository.Update(vehiculo);
+    }
+
+    public async Task Delete(int id)
+    {
+        var vehiculo = await _repository.GetById(id);
+
+        if (vehiculo == null)
+            throw new Exception("Vehiculo no encontrado");
+
+        await _repository.Delete(vehiculo);
+    }
+}
