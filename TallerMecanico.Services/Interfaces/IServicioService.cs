74754725eba51667b8b4296c36c12c08417fd4fd
Tairin-Munoz <tairinmunoz14@gmail.com>
2026-04-29 00:37:59 -0400

    feat: interfaz IServicioService

---
+++ TallerMecanico.Services/Interfaces/IServicioService.cs
@@ -0,0 +1,12 @@
+﻿using TallerMecanico.Core.Entities;
+
+namespace TallerMecanico.Services.Interfaces;
+
+public interface IServicioService
+{
+    Task<IEnumerable<Servicio>> GetAllAsync();
+    Task<Servicio> GetByIdAsync(int id);
+    Task Insert(Servicio servicio);
+    Task Update(Servicio servicio);
+    Task Delete(int id);
+}
