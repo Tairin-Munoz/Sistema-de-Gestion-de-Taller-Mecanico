9a69a62d6b21f36ff82f68a2e03ca17a9c3a4f60
Tairin-Munoz <tairinmunoz14@gmail.com>
2026-04-29 00:48:59 -0400

    feat: query SQL para obtener servicios con Dapper

---
+++ TallerMecanico.Infrastructure/Queries/ServicioQuery.cs
@@ -0,0 +1,8 @@
+﻿namespace TallerMecanico.Infrastructure.Queries;
+
+public static class ServicioQuery
+{
+    public static string GetAll = @"
+        SELECT Id, Nombre, Precio, Activo
+        FROM Servicio";
+}
