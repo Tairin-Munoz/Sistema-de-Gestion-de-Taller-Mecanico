7b890a48329d4b3b9d983eead31a403f95fa9276
Tairin-Munoz <tairinmunoz14@gmail.com>
2026-04-29 00:56:47 -0400

    feat: creación DTO OrdenTrabajo

---
+++ TallerMecanico.Core/DTOs/OrdenTrabajoDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace TallerMecanico.Core.DTOs;
+{
+    public class OrdenTrabajoDto
+    {
+        public int Id { get; set; }
+        public int VehiculoId { get; set; }
+        public int ServicioId { get; set; }
+        public string Fecha { get; set; }
+        public string Estado { get; set; }
+    }
+}
