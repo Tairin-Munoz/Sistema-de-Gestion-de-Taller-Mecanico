124ab9e1f5cfabf4e50c3c744f25f60b93584378
Tairin-Munoz <tairinmunoz14@gmail.com>
2026-04-29 00:54:49 -0400

    feat: creación entidad OrdenTrabajo

---
+++ TallerMecanico.Core/Entities/OrdenTrabajo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace TallerMecanico.Core.Entities
+{
+    public class OrdenTrabajo : BaseEntity
+    {
+        public int VehiculoId { get; set; }
+        public int ServicioId { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Estado { get; set; }
+
+        public Vehiculo Vehiculo { get; set; }
+        public Servicio Servicio { get; set; }
+    }
+}
