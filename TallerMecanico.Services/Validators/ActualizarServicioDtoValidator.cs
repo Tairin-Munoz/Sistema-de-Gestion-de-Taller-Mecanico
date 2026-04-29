f9bb3b9a71085aa72c4ddad66cb28ece30740844
Tairin-Munoz <tairinmunoz14@gmail.com>
2026-04-29 00:32:38 -0400

    feat: validaciones FluentValidation para Servicio (crear y actualizar)

---
+++ TallerMecanico.Services/Validators/ActualizarServicioDtoValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using TallerMecanico.Core.DTOs;
+
+namespace TallerMecanico.Services.Validators;
+
+public class ActualizarServicioDtoValidator : AbstractValidator<ServicioDto>
+{
+    public ActualizarServicioDtoValidator()
+    {
+        
+        RuleFor(x => x.Id)
+            .GreaterThan(0).WithMessage("El ID es obligatorio");
+
+        RuleFor(x => x.Nombre)
+            .NotEmpty()
+            .MinimumLength(3);
+
+        RuleFor(x => x.Precio)
+            .GreaterThan(0);
+
+        RuleFor(x => x.Activo)
+            .NotNull();
+    }
+}
